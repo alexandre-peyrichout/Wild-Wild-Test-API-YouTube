2bb53d3ff9e826eac4f4a40c5cc00eb2187d9353
Alexandre Peyrichout <a.peyrichout@gmail.com>
2019-10-25 15:25:32 +0200

    moved data to Components

---
+++ src/components/Game/CountDown.js
@@ -0,0 +1,1 @@
+//<Countdown date={Date.now() + 45000} renderer={props => <div>{props.seconds}</div>}
---
+++ src/components/Game/PlayerAPI.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+import Countdown from 'react-countdown-now';
+
+class PlayerAPI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoId: '',
+      opts: {
+        playerVars: {
+          autoplay: 1,
+          enablejsapi: 1,
+          origin: 'http://localhost:3000'
+        }
+      }
+    };
+    this.changeSong = this.changeSong.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <YouTube
+          className="yt-hidden"
+          videoId={this.state.videoId}
+          opts={this.state.opts}
+          onReady={this._onReady}
+        />
+        <button onClick={() => this.changeSong('KR1Uy47KdF4')}>Titanic</button>
+        <button onClick={() => this.changeSong('jRP5mpZVmN8')}>Friends</button>
+        <button onClick={() => this.changeSong('jVm1NbrXaXc')}>Pokémon</button>
+        <Countdown date={Date.now() + 45000} renderer={props => <div>{props.seconds}</div>} />
+      </div>
+    );
+  }
+
+  changeSong(video) {
+    this.setState({ videoId: video });
+  }
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.setVolume(100);
+  }
+}
+
+export default PlayerAPI;
