6dcf67ba8b57067f999152e61f088741e3f3f09a
harshbones <davranjar@hotmail.com>
2019-10-29 16:47:59 +0100

    set countdown to work with player

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import CountDown from './Game/CountDown';
-import PlayerAPI from './Game/PlayerAPI';
+// import PlayerAPI from './Game/PlayerAPI';
+import YouTube from 'react-youtube';
 import Score from './Game/Score';
 import SkipBtn from './Game/SkipBtn';
 import Answer from './Game/Answer';
 import ValidateBtn from './Game/ValidateBtn';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlaying: false,
+
+      height: '3',
+      width: '3',
+      videoId: '',
+      opts: {
+        playerVars: {
+          autoplay: 1,
+          start: 13,
+          enablejsapi: 1,
+          origin: 'http://localhost:3000'
+        }
+      }
+    };
+    this.changeSong = this.changeSong.bind(this);
+    this._onPlay = this._onPlay.bind(this);
+  }
+
+  changeSong(video) {
+    this.setState({ videoId: video });
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.setVolume(100);
+  }
+
+  _onPlay() {
+    this.setState({ isPlaying: true });
+  }
+
   render() {
     return (
       <div>
@@ -14,9 +49,22 @@ class Game extends React.Component {
           alt="placeholder"
           src="https://thumbs.gfycat.com/PalatableFelineDipper-size_restricted.gif"
         />
-        <CountDown />
 
-        <PlayerAPI />
+        <div>
+          <YouTube
+            className="yt-hidden"
+            height={this.state.height}
+            width={this.state.width}
+            videoId={this.state.videoId}
+            opts={this.state.opts}
+            onReady={this._onReady}
+            onPlay={this._onPlay}
+          />
+
+          <button onClick={() => this.changeSong('JvKoBnv96PM')}>Play a song</button>
+        </div>
+
+        <CountDown startCount={this.state.isPlaying} />
         <Answer />
         <ValidateBtn />
         <SkipBtn />
--- src/components/Game/CountDown.js
+++ src/components/Game/CountDown.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PlayerAPI from './PlayerAPI';
 
-class CountDown extends Component {
+class CountDown extends PlayerAPI {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.startCount = false;
+    this.state = {
+      number: 30
+    };
   }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.props.startCount
+      ? this.setState({ number: this.state.number - 1 })
+      : this.setState({ number: this.state.number });
+  }
+
   render() {
-    return <div>Compteur</div>;
+    return <div id="countdown-number">{this.state.number}</div>;
   }
 }
 
