43a3162ab12ec67425a6d5669f29d4f1651311b1
Alexandre Peyrichout <a.peyrichout@gmail.com>
2019-10-29 11:12:19 +0100

    added children to Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import CountDown from './Game/CountDown';
+import PlayerAPI from './Game/PlayerAPI';
+import Score from './Game/Score';
+import SkipBtn from './Game/SkipBtn';
+import Answer from './Game/Answer';
+import ValidateBtn from './Game/ValidateBtn';
 
 class Game extends React.Component {
   render() {
@@ -8,6 +14,12 @@ class Game extends React.Component {
           alt="placeholder"
           src="https://thumbs.gfycat.com/PalatableFelineDipper-size_restricted.gif"
         />
+        <CountDown />
+        <Score />
+        <PlayerAPI />
+        <Answer />
+        <ValidateBtn />
+        <SkipBtn />
       </div>
     );
   }
