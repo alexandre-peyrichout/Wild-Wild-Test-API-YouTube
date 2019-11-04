d628b5323b95fffc14fdbf1cc277a8f8a2e382fe
mrfouine <kas3rkin@gmail.com>
2019-11-04 12:00:10 +0100

    update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Title from './Home/Title';
 import Logo from './Home/Logo';
-import PlayBtn from './Home/PlayBtn';
+import './Home.css';
 import NickName from './Home/NickName';
 import RulesBtn from './Home/RulesBtn';
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="home">
         <RulesBtn />
         <Title />
         <Logo />
         <NickName />
-        <PlayBtn />
       </div>
     );
   }
