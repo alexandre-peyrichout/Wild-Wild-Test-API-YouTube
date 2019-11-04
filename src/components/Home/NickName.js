e80475bf1e01fb0941ee75671bab35daf5d7c8e8
mrfouine <kas3rkin@gmail.com>
2019-11-04 12:01:39 +0100

    update NickName.js

--- src/components/Home/NickName.js
+++ src/components/Home/NickName.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import './NickName.css';
+import PlayBtn from '../Home/PlayBtn';
 
 const MAX_LENGTH = 10;
 
 export default class NickName extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: '' };
+    this.state = { name: '', nameValidate: false };
   }
 
   handleChange = event => {
     event.target.value.length <= MAX_LENGTH && this.setState({ name: event.target.value });
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('test');
+    console.log(this.state.name);
+    const newNameValidate = !this.state.nameValidate;
+    this.state.name != '' && this.setState({ nameValidate: newNameValidate });
+  };
+
   render() {
     let max = this.state.name.length >= MAX_LENGTH;
+    console.log('nameValidate: ' + this.state.nameValidate);
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -27,7 +38,8 @@ export default class NickName extends React.Component {
               className={max ? 'length-maximum-reached' : 'length-ok'}
             />
           </label>
-          {/* <input type="submit" value="Envoyer" /> */}
+          {/* { <input type="submit" value="Envoyer" /> } */}
+          {this.state.nameValidate && <PlayBtn />}
         </form>
       </div>
     );
