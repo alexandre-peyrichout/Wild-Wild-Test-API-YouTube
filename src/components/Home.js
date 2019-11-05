d7d6df3872cbfbb610aa28c57cb754aaffb8749f
Chloe Gimenes <chloegimenes@gmail.com>
2019-11-05 09:43:34 +0100

    lastcommitduselect

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,24 @@ import Title from './Home/Title';
 import Logo from './Home/Logo';
 import PlayBtn from './Home/PlayBtn';
 import NickName from './Home/NickName';
+import ChooseTheme from './Home/ChooseTheme';
 import RulesBtn from './Home/RulesBtn';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idSongs: ''
+    };
+    this.goToPlaylist = this.goToPlaylist.bind(this);
+  }
+
+  goToPlaylist(value) {
+    this.setState({
+      idSongs: value
+    });
+  }
+
   render() {
     return (
       <div>
@@ -13,6 +28,7 @@ class Home extends React.Component {
         <Title />
         <Logo />
         <NickName />
+        <ChooseTheme parentMethodChoosePlaylist={this.goToPlaylist} value={this.state.idSongs} />
         <PlayBtn />
       </div>
     );
