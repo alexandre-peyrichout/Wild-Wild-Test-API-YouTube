cd61f6a6dc203a9e72fbaec0ec199c280328d099
Chloe Gimenes <chloegimenes@gmail.com>
2019-11-06 16:05:03 +0100

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Contact.css';
 
 class Contact extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" className="lname" name="lastname" />
-          <input type="text" className="write" name="write" />
-          <div>
-            <button></button>
-          </div>
-        </form>
+      <div class="container-form">
+        <div class="contactform">
+          <img src="../assets/img/logo.png" alt="logo" />
+          <p>Hi (pseudo), contact us !</p>
+          <input type="email" placeholder="write your email" />
+          <textarea className="textarea" type="textarea" placeholder="Tell us a joke"></textarea>
+          <button>SEND !</button>
+        </div>
       </div>
     );
   }
