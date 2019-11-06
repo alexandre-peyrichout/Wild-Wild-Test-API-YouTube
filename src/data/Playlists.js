a7531a4d5c021b7c7cd324eb94642927caa6fc85
harshbones <davranjar@hotmail.com>
2019-11-06 15:06:02 +0100

    added playlists : leagueChampions, teenmovies, wildwildwest

---
+++ src/data/Playlists.js
@@ -0,0 +1,422 @@
+import React from 'react';
+
+export const leagueChampions = [
+  {
+    id: 1,
+    name: 'Ekko, the Boy Who Shattered Time',
+    author: '',
+    year: '2015',
+    length: '',
+    url: '8_OSWJeVTg0',
+    pic: '',
+    answers: ['Ekko', 'ekko']
+  },
+  {
+    id: 2,
+    name: 'Pyke, the Bloodharbor Ripper',
+    author: '',
+    year: '2018',
+    length: '',
+    url: 'cHRvLl8hpqc',
+    pic: '',
+    answers: ['Pyke', 'pyke']
+  },
+  {
+    id: 3,
+    name: 'Jhin, the Virtuoso',
+    author: '',
+    year: '2016',
+    length: '',
+    url: 'NSQZqVsaKWY',
+    pic: '',
+    answers: ['Jhin', 'jhin', 'khada jhin', 'Khada Jhin', 'Khada jhin', 'khada Jhin']
+  },
+  {
+    id: 4,
+    name: 'Thresh, the Chain Warden',
+    author: '',
+    year: '2013',
+    length: '',
+    url: '6WyH5E8Xs0c',
+    pic: '',
+    answers: ['Thresh', 'thresh']
+  },
+  {
+    id: 5,
+    name: 'Jinx, the Loose Cannon',
+    author: '',
+    year: '2013',
+    length: '',
+    url: 'JoHRzfKrdtk',
+    pic: '',
+    answers: ['Jinx', 'jinx']
+  },
+  {
+    id: 6,
+    name: 'Kled, the Cantankerous Cavalier',
+    author: '',
+    year: '2016',
+    length: '',
+    url: '-P9zLHDUFEM',
+    pic: '',
+    answers: ['Kled', 'kled']
+  },
+  {
+    id: 7,
+    name: 'Braum, the Heart of Freljord',
+    author: '',
+    year: '2016',
+    length: '',
+    url: 'F8cPDpXnQa0',
+    pic: '',
+    answers: ['Braum', 'braum']
+  },
+  {
+    id: 8,
+    name: 'Draven, the Glorious Executioner',
+    author: 'Draven',
+    year: 'Draven',
+    length: '',
+    url: 'H1xHayXetLU',
+    pic: '',
+    answers: ['Draven', 'draven', 'tyler1', 'Tyler1', 'tyler', 'Tyler']
+  },
+  {
+    id: 9,
+    name: "Rek'Sai, the Void Burrower",
+    author: '',
+    year: '2014',
+    length: '',
+    url: 'zXXdL4rHvww',
+    pic: '',
+    answers: ["Rek'Sai", "Rek'sai", "rek'sai", 'reksai', 'RekSai', 'rek sai', 'Rek Sai', 'Rek sai']
+  },
+  {
+    id: 10,
+    name: 'Tahm Kench, the River King',
+    author: '',
+    year: '2015',
+    length: '',
+    url: 'lr4EqywQGTQ',
+    pic: '',
+    answers: ['Tahm Kench', 'Tahm kench', 'tahm kench', 'Tahmkench', 'TahmKench', 'tahmkench']
+  }
+];
+
+export const teenmovies = [
+  {
+    id: 1,
+    name: 'American Pie',
+    author: '',
+    year: '',
+    length: '',
+    url: '4PgNhsD5bsU',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 2,
+    name: 'High School Musical',
+    author: '',
+    year: '',
+    length: '',
+    url: 'dEOVNB0hXjo',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 3,
+    name: 'Twilight',
+    author: '',
+    year: '',
+    length: '',
+    url: 'Z7-3_h0vxXA',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 4,
+    name: 'Scary Movie',
+    author: '',
+    year: '',
+    length: '',
+    url: 'bYceRXYdyyI',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 5,
+    name: 'Sex Intention',
+    author: '',
+    year: '',
+    length: '',
+    url: 'HgS0o2-ZYW4',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 6,
+    name: 'Projet X',
+    author: '',
+    year: '',
+    length: '',
+    url: 'CVFkvTKfcqk',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 7,
+    name: 'Virgin Suicide',
+    author: '',
+    year: '',
+    length: '',
+    url: 'hFuu5wPFv1M',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 8,
+    name: 'Rasta Rocket',
+    author: '',
+    year: '',
+    length: '',
+    url: 'MrHxhQPOO2c',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 9,
+    name: 'Harry Potter',
+    author: '',
+    year: '',
+    length: '',
+    url: 'Htaj3o3JD8I',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 10,
+    name: 'Very Bad Trip',
+    author: '',
+    year: '',
+    length: '',
+    url: 'CcCw1ggftuQ',
+    pic: '',
+    answers: []
+  }
+];
+
+export const wildwildwest = [
+  {
+    id: 1,
+    name: 'A Fistful of Dollars',
+    author: 'Ennio Morricone',
+    year: '1965',
+    length: '',
+    url: 'HjjDOdaFZg0',
+    pic: '',
+    answers: ['pour une poignée de dollars', 'pour une poignee de dollars', 'a fistful of dollars']
+  },
+  {
+    id: 2,
+    name: 'The Good, The Bad and The Ugly',
+    author: 'Ennio Morricone',
+    year: '1966',
+    length: '',
+    url: 'LdLQf1Ef9Ns',
+    pic: '',
+    answers: [
+      'the good, the bad and the ugly',
+      'the good the bad and the ugly',
+      'le bon, la brute et le truand',
+      'le bon la brute et le truand'
+    ]
+  },
+  {
+    id: 3,
+    name: 'Django Unchained',
+    author: 'Luis Bacalov',
+    year: '',
+    length: '',
+    url: 'IAooXLAPoBQ',
+    pic: '',
+    answers: ['django', 'django unchained']
+  },
+  {
+    id: 4,
+    name: 'Once Upon A Time In The West',
+    author: 'Ennio Morricone',
+    year: '',
+    length: '',
+    url: 'm6BQKFs3-VM',
+    pic: '',
+    answers: [
+      'once upon a time in the west',
+      "il était une fois dans l'ouest",
+      "l'uomo dell'armonica"
+    ]
+  },
+  {
+    id: 5,
+    name: 'For a Few Dollars More',
+    author: 'Ennio Morricone',
+    year: '',
+    length: '',
+    url: 'J84FPJAb7rM',
+    pic: '',
+    answers: ['for a few dollars more', 'et pour quelques dollars de plus']
+  },
+  {
+    id: 6,
+    name: 'Rango',
+    author: 'Los Lobos',
+    year: '',
+    length: '',
+    url: 'HsXnH0ly1S4',
+    pic: '',
+    answers: ['rango']
+  },
+  {
+    id: 7,
+    name: 'Rawhide',
+    author: 'Frankie Laine',
+    year: '',
+    length: '',
+    url: 'q_xaTxzfEZk',
+    pic: '',
+    answers: ['rawhide']
+  },
+  {
+    id: 8,
+    name: 'My Name is Nobody',
+    author: 'Ennio Morricone',
+    year: '',
+    length: '',
+    url: 'iGZDKuDl3jc',
+    pic: '',
+    answers: ['my name is nobody', 'mon nom est personne']
+  },
+  {
+    id: 9,
+    name: 'Brokeback Mountain - The Wings',
+    author: '',
+    year: '',
+    length: '',
+    url: '0qzMsJ-tmJY',
+    pic: '',
+    answers: ['brokeback mountain', 'the wings']
+  },
+  {
+    id: 10,
+    name: 'Westworld',
+    author: 'Ramin Djawadi',
+    year: '',
+    length: '',
+    url: 'rYelEUVQ50g',
+    pic: '',
+    answers: ['westworld']
+  }
+];
+
+export const template = [
+  {
+    id: 1,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 2,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 3,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 4,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 5,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 6,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 7,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 8,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 9,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  },
+  {
+    id: 10,
+    name: '',
+    author: '',
+    year: '',
+    length: '',
+    url: '',
+    pic: '',
+    answers: []
+  }
+];
