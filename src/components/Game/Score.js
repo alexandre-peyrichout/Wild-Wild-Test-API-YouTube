23c685db63a4873f89dfb9430c8bf419e0a7203a
mrfouine <kas3rkin@gmail.com>
2019-11-13 10:17:40 +0100

    add cumulate score and some animation

--- src/components/Game/Score.js
+++ src/components/Game/Score.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Score.css';
 
-const Score = () => {
-  return <p className="score">Score:</p>;
-};
+class Score extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class={this.props.transferAnswerState ? 'ball spring' : 'ball'}>
+        <p className="score">Score: {this.props.transferScore} </p>
+      </div>
+    );
+  }
+}
 
 export default Score;
